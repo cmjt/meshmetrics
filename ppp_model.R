f94b50b0087c716ff2fa92a2134770b5156d7561
Astronautspencer <spencer.hshi@outlook.com>
2022-03-15 21:42:48 +1300

    fit the model with multiple meshes

--- ppp_model.R
+++ ppp_model.R
@@ -10,11 +10,15 @@ library(dplyr)
 library(patchwork)
 library(maptools)
 source("functions.r")
-source("ppp_sim.R") ## generate 8 different point patterns
-source("ppp_mesh.R") ## one mesh was made using point locations from 8 different point patterns
+## generate 8 different point patterns
+source("ppp_sim.R")
+## 4 mesh were made using point locations from 8 different point patterns
+source("ppp_mesh.R")
 
 
-## ---- fitting the model, 8 models in total
+## ---- fitting the model, 32 models in total
+
+mesh_flatten <- unlist(mesh_list, recursive = FALSE)
 
 n <- list()
 nv <- list()
@@ -33,63 +37,70 @@ stk.pp <- list()
 pp.res <- list()
 pp.res.est <- list()
 
-for (m in 1:length(mesh_ls)) {
+for (p in 1:(length(mesh_flatten)/length(mesh_list))) {
   
   ## number of simulated points
   
-  n[[m]] <- nrow(points_df[[m]]) 
+  n[[p]] <- nrow(points_df[[p]])
+}
+
+n <- rep(unlist(n), length(mesh_list))
+
+
+for (q in 1:length(mesh_flatten)) {
   
   ## number of vertices in the mesh
   
-  nv[[m]] <- mesh_ls[[m]]$n
+  nv[[q]] <- mesh_flatten[[q]]$n
   
   ## define the SPDE model
   
-  spde[[m]] <- inla.spde2.pcmatern(mesh = mesh_ls[[m]],
+  spde[[q]] <- inla.spde2.pcmatern(mesh = mesh_flatten[[q]],
                                    alpha = 2,
                                    prior.range = c(0.1, 0.01), # P(range < 0.1) = 0.01
                                    prior.sigma = c(0.01, 0.01)) # P(sigma > 0.01) = 0.01)
   
   ## mesh weights
   
-  dmesh[[m]] <- book.mesh.dual(mesh_ls[[m]])
+  dmesh[[q]] <- book.mesh.dual(mesh_flatten[[q]])
   domainSP <- SpatialPolygons(list(Polygons(list(Polygon(domain)), '0')))
   
-  w[[m]] <- sapply(1:length(dmesh[[m]]), function(i) {
-    if (gIntersects(dmesh[[m]][i, ], domainSP)) 
-      return(gArea(gIntersection(dmesh[[m]][i, ], domainSP)))
+  w[[q]] <- sapply(1:length(dmesh[[q]]), function(i) {
+    if (gIntersects(dmesh[[q]][i, ], domainSP)) 
+      return(gArea(gIntersection(dmesh[[q]][i, ], domainSP)))
     else 
       return(0) 
   })
   
   ## projector matrix 
   
-  y.pp[[m]] <- rep(0:1, c(nv[[m]], n[[m]]))
-  e.pp[[m]] <- c(w[[m]], rep(0, n[[m]]))
-  imat[[m]] <- Diagonal(nv[[m]], rep(1, nv[[m]]))
-  lmat[[m]] <- inla.spde.make.A(mesh = mesh_ls[[m]], loc = coordinates(points_df[[m]]))
-  A.pp[[m]] <- rbind(imat[[m]], lmat[[m]])
+  y.pp[[q]] <- rep(0:1, c(nv[[q]], n[q]))
+  e.pp[[q]] <- c(w[[q]], rep(0, n[q]))
+  imat[[q]] <- Diagonal(nv[[q]], rep(1, nv[[q]]))
+  lmat[[q]] <- inla.spde.make.A(mesh = mesh_flatten[[q]], 
+                                loc = coordinates(rep(points_df, length(mesh_list))[[q]]))
+  A.pp[[q]] <- rbind(imat[[q]], lmat[[q]])
   
   ## the data stack
   
-  stk.pp[[m]] <- inla.stack(
-    data = list(y = y.pp[[m]], e = e.pp[[m]]),
-    A = list(1, A.pp[[m]]),
-    effects = list(list(b0 = rep(1, nv[[m]] + n[[m]])), list(i = 1:nv[[m]])),
+  stk.pp[[q]] <- inla.stack(
+    data = list(y = y.pp[[q]], e = e.pp[[q]]),
+    A = list(1, A.pp[[q]]),
+    effects = list(list(b0 = rep(1, nv[[q]] + n[q])), list(i = 1:nv[[q]])),
     tag = 'pp')
   
   ## fit the model
   
-  pp.res[[m]] <- inla(y ~ 0 + b0 + f(i, model = spde[[m]]),
+  pp.res[[q]] <- inla(y ~ 0 + b0 + f(i, model = spde[[q]]),
                       family = 'poisson', 
-                      data = inla.stack.data(stk.pp[[m]]),
-                      control.predictor = list(A = inla.stack.A(stk.pp[[m]])),
+                      data = inla.stack.data(stk.pp[[q]]),
+                      control.predictor = list(A = inla.stack.A(stk.pp[[q]])),
                       control.compute = list(dic = TRUE, cpo = TRUE, waic = TRUE),
-                      E = inla.stack.data(stk.pp[[m]])$e)
+                      E = inla.stack.data(stk.pp[[q]])$e)
   
   ## transform to user scale
   
-  pp.res.est[[m]] <- inla.spde.result(inla = pp.res[[m]], name = "i", spde = spde[[m]], do.transf = TRUE)
+  pp.res.est[[q]] <- inla.spde.result(inla = pp.res[[q]], name = "i", spde = spde[[q]], do.transf = TRUE)
   
 }
 
@@ -105,7 +116,7 @@ list_dic <- list()
 list_waic <- list()
 
 
-for (f in 1:nrow(mesh_mat)){
+for (f in 1:length(mesh_flatten)){
   list_fixed[[f]] <- pp.res[[f]]$summary.fixed
   marg_fixed[[f]] <- pp.res[[f]]$marginals.fixed[[1]]
   marg_kappa[[f]] <- pp.res.est[[f]]$marginals.kappa[[1]]
@@ -118,45 +129,45 @@ for (f in 1:nrow(mesh_mat)){
 ### --------------------------------------------------------------------------->> CSV summary.fixed
 fixed <- data.frame(matrix(unlist(list_fixed), ncol = 7, byrow = TRUE))
 colnames(fixed) <- names(pp.res[[1]]$summary.fixed)
-fixed$Mesh <- paste("Mesh", 1:nrow(mesh_mat), sep = "_")
+fixed$Mesh <- paste("Mesh", 1:length(mesh_list), sep = "_")
 write.csv(fixed, file = "summary_fixed.csv")
 
 
 ### --------------------------------------------------------------------------->> CSV marginals.fixed
 margfixed <- data.frame(do.call('rbind', marg_fixed))
-margfixed$Mesh <- rep(paste("Mesh", 1:nrow(mesh_mat), sep = "_"), each = nrow(pp.res[[1]]$marginals.fixed[[1]]))
+margfixed$Mesh <- rep(paste("Mesh", 1:length(mesh_list), sep = "_"), each = nrow(pp.res[[1]]$marginals.fixed[[1]]))
 write.csv(margfixed, file = "marginals_fixed.csv")
 
 
 ### --------------------------------------------------------------------------->> CSV marginals.log.kappa
 margkappa <- data.frame(do.call('rbind', marg_kappa))
-margkappa$Mesh <- rep(paste("Mesh", 1:nrow(mesh_mat), sep = "_"), each = nrow(pp.res.est[[1]]$marginals.kappa[[1]]))
+margkappa$Mesh <- rep(paste("Mesh", 1:length(mesh_list), sep = "_"), each = nrow(pp.res.est[[1]]$marginals.kappa[[1]]))
 write.csv(margkappa, file = "marginals_kappa.csv")
 
 
 ### --------------------------------------------------------------------------->> CSV marginals.log.variance.nominal
 margvar <- data.frame(do.call('rbind', marg_variance))
-margvar$Mesh <- rep(paste("Mesh", 1:nrow(mesh_mat), sep = "_"), each = nrow(pp.res.est[[1]]$marginals.variance.nominal[[1]]))
+margvar$Mesh <- rep(paste("Mesh", 1:length(mesh_list), sep = "_"), each = nrow(pp.res.est[[1]]$marginals.variance.nominal[[1]]))
 write.csv(margvar, file = "marginals_variance.csv")
 
 
 ### --------------------------------------------------------------------------->> CSV marginals.log.range.nominal
 margrange <- data.frame(do.call('rbind', marg_range))
-margrange$Mesh <- rep(paste("Mesh", 1:nrow(mesh_mat), sep = "_"), each = nrow(pp.res.est[[1]]$marginals.range.nominal[[1]]))
+margrange$Mesh <- rep(paste("Mesh", 1:length(mesh_list), sep = "_"), each = nrow(pp.res.est[[1]]$marginals.range.nominal[[1]]))
 write.csv(margrange, file = "marginals_range.csv")
 
 
 ### --------------------------------------------------------------------------->> CSV DIC
 dic <- data.frame(matrix(unlist(list_dic), ncol = 1, byrow = TRUE))
 colnames(dic) <- "DIC"
-dic$Mesh <- paste("Mesh", 1:nrow(mesh_mat), sep = "_")
+dic$Mesh <- paste("Mesh", 1:length(mesh_list), sep = "_")
 write.csv(dic, file = "DIC.csv")
 
 
 ### --------------------------------------------------------------------------->> CSV WAIC
 waic <- data.frame(matrix(unlist(list_waic), ncol = 1, byrow = TRUE))
 colnames(waic) <- "WAIC"
-waic$Mesh <- paste("Mesh", 1:nrow(mesh_mat), sep = "_")
+waic$Mesh <- paste("Mesh", 1:length(mesh_list), sep = "_")
 write.csv(waic, file = "WAIC.csv")
 
 
